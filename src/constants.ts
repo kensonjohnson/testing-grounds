0590a35535533e7129bba99e2730b164c0730b69
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-10 13:55:55 -0500

    Add stripe variables

--- src/constants.ts
+++ src/constants.ts
@@ -1,27 +1,64 @@
+import assert from "assert";
 import { config } from "dotenv";
 config();
 
-const isProduction = process.env.NODE_ENV !== "development";
+assert(process.env.BASE_URL, "Environment variable BASE_URL must be set!");
+export const BASE_URL = process.env.BASE_URL;
+export const PORT = process.env.PORT || 3000;
 
-export const PORT = parseInt(process.env.PORT || "3000");
-
-const BASE_URL = process.env.BASE_URL ?? "http://localhost";
-
-export const WEBSITE_URL = isProduction ? BASE_URL : `http://localhost:${PORT}`;
-
+assert(
+  process.env.MAILTRAP_ENDPOINT,
+  "Environment variable MAILTRAP_ENDPOINT must be set!"
+);
+assert(
+  process.env.MAILTRAP_API_KEY,
+  "Environment variable MAILTRAP_API_KEY must be set!"
+);
 export const MAILTRAP_CONFIG = {
-  endpoint: process.env.MAILTRAP_ENDPOINT!,
-  token: process.env.MAILTRAP_API_KEY!,
+  endpoint: process.env.MAILTRAP_ENDPOINT,
+  token: process.env.MAILTRAP_API_KEY,
 };
 
-export const MAILTRAP_SENDER_EMAIL = process.env.MAILTRAP_SENDER_EMAIL!;
+assert(
+  process.env.MAILTRAP_SENDER_EMAIL,
+  "Environment variable MAILTRAP_SENDER_EMAIL must be set!"
+);
+export const MAILTRAP_SENDER_EMAIL = process.env.MAILTRAP_SENDER_EMAIL;
 
-export const SMTP_EMAIL = process.env.SMTP_EMAIL!;
-
+assert(
+  process.env.DATABASE_URL,
+  "Environment variable DATABASE_URL must be set!"
+);
 export const DB_CONFIG = {
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_URL,
 };
 
-export const COOKIE_SECRET = process.env.COOKIE_SECRET!;
+assert(
+  process.env.COOKIE_SECRET,
+  "Environment variable COOKIE_SECRET must be set!"
+);
+export const COOKIE_SECRET = process.env.COOKIE_SECRET;
 
-export const OPENAI_API_KEY = process.env.OPENAI_API_KEY!;
+assert(
+  process.env.OPENAI_API_KEY,
+  "Environment variable OPENAI_API_KEY must be set!"
+);
+export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+assert(
+  process.env.STRIPE_PUBLISHABLE_KEY,
+  "Environment variable STRIPE_PUBLISHABLE_KEY must be set!"
+);
+export const STRIPE_PUBLISHABLE_KEY = process.env.STRIPE_PUBLISHABLE_KEY;
+
+assert(
+  process.env.STRIPE_SECRET_KEY,
+  "Environment variable STRIPE_SECRET_KEY must be set!"
+);
+export const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+
+assert(
+  process.env.STRIPE_WEBHOOK_SECRET,
+  "Environment variable STRIPE_WEBHOOK_SECRET must be set!"
+);
+export const STRIPE_WEBHOOK_SECRET = process.env.STRIPE_WEBHOOK_SECRET;
