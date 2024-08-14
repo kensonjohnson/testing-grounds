48e2b35949ffec02ebe27038c06a688feb7a3319
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-13 20:17:12 -0500

    Handle checkout

--- frontend/routes/Account/Account.tsx
+++ frontend/routes/Account/Account.tsx
@@ -1,6 +1,14 @@
-import { Form, RouteObject, redirect, useLoaderData } from "react-router-dom";
+import {
+  Form,
+  Link,
+  RouteObject,
+  redirect,
+  useLoaderData,
+} from "react-router-dom";
 import { authProvider } from "../../providers/auth-provider";
 import styles from "./Account.module.css";
+import { stripeProvider } from "../../providers/stripe-provider";
+import { Checkout, loader as createSubscriptionLoader } from "./CheckoutForm";
 
 export const accountRoutes: RouteObject[] = [
   {
@@ -8,6 +16,11 @@ export const accountRoutes: RouteObject[] = [
     element: <Account />,
     loader,
     action,
+  },
+  {
+    path: "account/create-subscription",
+    element: <Checkout />,
+    loader: createSubscriptionLoader,
   },
 ];
 
@@ -31,11 +44,51 @@ async function loader() {
   if (!authProvider.isAuthenticated) {
     return redirect("/");
   }
-  return authProvider.user;
+
+  if (!stripeProvider.ready) {
+    const stipeConfigResponse = await fetch("/checkout/config");
+    const stripeConfig = await stipeConfigResponse.json();
+    await stripeProvider.init(stripeConfig);
+  }
+
+  let message: string | undefined = undefined;
+  if (stripeProvider.session) {
+    const response = await fetch(
+      "/checkout/session?sessionId=" + stripeProvider.session.id
+    );
+
+    if (!response.ok) {
+      message = "There was an error processing your request.";
+    }
+
+    const session = await response.json();
+
+    // Customer is still in the checkout process
+    if (session.status === "open") {
+      return redirect("/account/create-subscription");
+    }
+
+    // Customer has completed the checkout process, show a message
+    if (session.status === "completed") {
+      message = "Your subscription was successful!";
+    }
+
+    if (session.status === "failed") {
+      message = "Your subscription was not created.";
+    }
+
+    // Clear the session
+    stripeProvider.setSession(null);
+  }
+
+  return { user: authProvider.user, message };
 }
 
 export function Account() {
-  const user = useLoaderData() as User | null;
+  const { user } = useLoaderData() as {
+    user: User | null;
+    message: string | undefined;
+  };
 
   if (!user) {
     return <div>Loading...</div>;
@@ -61,8 +114,13 @@ export function Account() {
         {user.first_name && <h2>Welcome {user.first_name}!</h2>}
         <p>This is your account page. You can see your account details here.</p>
         <div className={styles.buttonContainer}>
+          <Link to="/account/create-subscription" className={styles.button}>
+            Create a Subscription
+          </Link>
           <Form method="post">
-            <button type="submit">Add Credits</button>
+            <button type="submit" className={styles.button}>
+              Add Credits Manually
+            </button>
           </Form>
         </div>
         <p>Credits last for 60 days</p>
---
+++ frontend/routes/Account/CheckoutForm.tsx
@@ -0,0 +1,53 @@
+import type Stripe from "stripe";
+import {
+  EmbeddedCheckoutProvider,
+  EmbeddedCheckout,
+} from "@stripe/react-stripe-js";
+import { useLoaderData } from "react-router-dom";
+import { stripeProvider } from "../../providers/stripe-provider";
+
+export async function loader() {
+  if (!stripeProvider.ready) {
+    const stipeConfigResponse = await fetch("/checkout/config");
+    const stripeConfig = await stipeConfigResponse.json();
+    await stripeProvider.init(stripeConfig);
+  }
+
+  if (!stripeProvider.session) {
+    const response = await fetch("/checkout/session/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        priceId: stripeProvider.subscriptionPriceId,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error("There was an error creating the checkout session.");
+    }
+
+    const { session } = await response.json();
+    stripeProvider.setSession(session);
+  }
+
+  return stripeProvider.session;
+}
+
+export function Checkout() {
+  const session = useLoaderData() as Stripe.Checkout.Session;
+
+  if (!session) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <EmbeddedCheckoutProvider
+      stripe={stripeProvider.stripe}
+      options={{ clientSecret: session.client_secret }}
+    >
+      <EmbeddedCheckout />
+    </EmbeddedCheckoutProvider>
+  );
+}
