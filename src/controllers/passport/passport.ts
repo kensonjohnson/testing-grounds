ea50f2fbb349b26a59e427ed4f66a42262287655
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-08-13 19:32:23 -0500

    Update import

--- src/controllers/passport/passport.ts
+++ src/controllers/passport/passport.ts
@@ -2,7 +2,7 @@ import type { Express } from "express";
 import { MagicLinkStrategy } from "./magic-link.js";
 import session from "express-session";
 import connectPG from "connect-pg-simple";
-import { pool as pgPool } from "../../db/db.js";
+import { pool as pgPool } from "../../drizzle/db.js";
 import passport from "passport";
 import { COOKIE_SECRET } from "../../constants.js";
 
